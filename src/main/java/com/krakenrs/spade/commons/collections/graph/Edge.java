a52c0efbd8ec1b23d8678348c81935e16c03862b
Matt Smith <matt@offtopica.uk>
2020-04-09 12:13:20 +0100

    graph: Implement Edge.toString
    Also implement TestVertex.toString

--- src/main/java/com/krakenrs/spade/commons/collections/graph/Edge.java
+++ src/main/java/com/krakenrs/spade/commons/collections/graph/Edge.java
@@ -43,6 +43,14 @@ public class Edge<V> {
     }
 
     @Override
+    public String toString() {
+        return "Edge{" +
+                "source=" + source +
+                ", destination=" + destination +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
--- src/test/java/com/krakenrs/spade/commons/collections/graph/TestVertex.java
+++ src/test/java/com/krakenrs/spade/commons/collections/graph/TestVertex.java
@@ -14,6 +14,11 @@ public class TestVertex implements Vertex {
     }
 
     @Override
+    public String toString() {
+        return String.format("TestVertex #%d", id);
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
