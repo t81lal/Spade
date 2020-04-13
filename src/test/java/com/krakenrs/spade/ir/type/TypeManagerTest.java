5245e8ba59a7af9a781bc7b21124d63b9569a311
Matt Smith <matt@offtopica.uk>
2020-04-13 10:18:38 +0100

    ir: type: Add tests for TypeManager.lca

--- src/test/java/com/krakenrs/spade/ir/type/TypeManagerTest.java
+++ src/test/java/com/krakenrs/spade/ir/type/TypeManagerTest.java
@@ -1,5 +1,6 @@
 package com.krakenrs.spade.ir.type;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -12,6 +13,7 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TypeManagerTest {
     final Supplier<MockTypeManager> typeManagerSupplier = MockTypeManager::new;
@@ -20,8 +22,22 @@ class TypeManagerTest {
         return Stream.of(
                 Arguments.of(new MethodType(List.of(), PrimitiveType.VOID), "()V"),
                 Arguments.of(new MethodType(List.of(), PrimitiveType.INT), "()I"),
-                Arguments.of(new MethodType(List.of(PrimitiveType.INT, PrimitiveType.INT), 
-                                                    PrimitiveType.INT), "(II)I"));
+                Arguments.of(new MethodType(List.of(PrimitiveType.INT, PrimitiveType.INT),
+                        PrimitiveType.INT), "(II)I"));
+    }
+
+    private static Stream<Arguments> ancestors() {
+        return Stream.of(
+                Arguments.of("java.lang.Object", "java.lang.Object", "java.lang.Object"),
+                Arguments.of("java.lang.Comparable", "java.io.Serializable", "java.lang.Object"),
+                Arguments.of("java.lang.Integer", "java.lang.Number", "java.lang.Number"),
+                Arguments.of("java.lang.Number", "java.lang.Integer", "java.lang.Number"),
+                Arguments.of("java.lang.Integer", "java.lang.Long", "java.lang.Comparable"),
+                Arguments.of("java.lang.Long", "java.lang.Integer", "java.lang.Comparable"),
+                Arguments.of("foo.Unresolved", "java.lang.Integer", "java.lang.Object"),
+                Arguments.of("java.lang.Integer", "foo.Unresolved", "java.lang.Object"),
+                Arguments.of("foo.CoolInteger", "java.lang.Long", "java.lang.Number"),
+                Arguments.of("foo.CoolInteger", "java.lang.Integer", "java.lang.Integer"));
     }
 
     @ParameterizedTest(name = "[{index}] {1}")
@@ -32,11 +48,42 @@ class TypeManagerTest {
         assertEquals(expected, actual);
     }
 
+    @ParameterizedTest(name = "[{index}] {0}, {1}")
+    @MethodSource("ancestors")
+    void testLca(String lhsDesc, String rhsDesc, String expectedDesc) {
+        final var typeManager = typeManagerSupplier.get();
+
+        // TODO: Use org.junit.jupiter.params.converter.ArgumentConverter.
+        final var lhs = typeManager.asClassType(lhsDesc);
+        final var rhs = typeManager.asClassType(rhsDesc);
+        final var expected = typeManager.asClassType(expectedDesc);
+
+        final var actual = typeManager.lca(lhs, rhs);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testLcaInvalid() {
+        final var typeManager = typeManagerSupplier.get();
+        assertThrows(NullPointerException.class, () -> typeManager.lca(null, null));
+    }
+
     static class MockTypeManager extends TypeManager {
         private final Map<String, ClassType> classes = new HashMap<>();
 
         public MockTypeManager() {
             addClass("java/lang/Object", new ResolvedClassType("java/lang/Object", null, Set.of()));
+            addClass("java/io/Serializable", new ResolvedClassType("java/io/Serializable", null, Set.of()));
+            addClass("java/lang/Comparable", new ResolvedClassType("java/lang/Comparable", null, Set.of()));
+            addClass("java/lang/Number", new ResolvedClassType("java/lang/Number", findClassType("java/lang/Object"),
+                    Set.of(findClassType("java/io/Serializable"))));
+            addClass("java/lang/Integer", new ResolvedClassType("java/lang/Integer", findClassType("java/lang/Number"),
+                    Set.of(findClassType("java/lang/Comparable"))));
+            addClass("java/lang/Long", new ResolvedClassType("java/lang/Long", findClassType("java/lang/Number"),
+                    Set.of(findClassType("java/lang/Comparable"))));
+
+            addClass("foo/CoolInteger",
+                    new ResolvedClassType("foo/CoolInteger", findClassType("java/lang/Integer"), Set.of()));
         }
 
         public void addClass(String className, ClassType classType) {
