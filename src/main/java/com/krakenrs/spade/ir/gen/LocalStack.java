cd566c3f611d40251845a47d08577a0ffac8aee9
t81lal <bilal.a.tariq@gmail.com>
2020-04-13 19:37:52 +0100

    ir: gen: add LocalStack + tests

---
+++ src/main/java/com/krakenrs/spade/ir/gen/LocalStack.java
@@ -0,0 +1,168 @@
+package com.krakenrs.spade.ir.gen;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import com.krakenrs.spade.commons.collections.tuple.Tuple2.ImmutableTuple2;
+import com.krakenrs.spade.ir.type.ValueType;
+import com.krakenrs.spade.ir.value.Local;
+
+public class LocalStack {
+
+    private TypedLocal[] stack;
+    private int size;
+
+    public LocalStack() {
+        this(8 * 8);
+    }
+
+    public LocalStack(int capacity) {
+        capacity = Math.max(capacity, 1);
+        stack = new TypedLocal[capacity];
+        size = 0;
+    }
+
+    private void expand() {
+        TypedLocal[] s = new TypedLocal[size * 2];
+        System.arraycopy(stack, 0, s, 0, size);
+        stack = s;
+    }
+
+    public void push(TypedLocal e) {
+        int i = size++;
+        if (stack.length == size) {
+            expand();
+        }
+        stack[i] = e;
+    }
+
+    public TypedLocal peek() {
+        int index = size - 1;
+        if (index < 0) {
+            throw new UnsupportedOperationException("Cannot peek on empty stack");
+        }
+        return stack[index];
+    }
+
+    public TypedLocal peek(int d) {
+        if (d < 0) {
+            throw new IllegalArgumentException("depth must be positive");
+        }
+        int index = size - d - 1;
+        if (index < 0) {
+            throw new UnsupportedOperationException("Cannot peek on empty stack");
+        }
+        return stack[index];
+    }
+
+    public TypedLocal pop() {
+        if (size == 0) {
+            throw new UnsupportedOperationException("Cannot pop from empty stack");
+        }
+        TypedLocal e = stack[--size];
+        stack[size] = null;
+        return e;
+    }
+
+    public TypedLocal getAt(int i) {
+        if(i < 0) {
+            throw new IllegalArgumentException("Index must be positive");
+        } else if(i >= size) {
+            throw new IllegalArgumentException("Index falls off the end of the stack");
+        }
+        return stack[i];
+    }
+
+    public void copyInto(LocalStack other) {
+        TypedLocal[] news = new TypedLocal[capacity()];
+        System.arraycopy(stack, 0, news, 0, capacity());
+        other.stack = news;
+        other.size = size;
+    }
+
+    public LocalStack copy() {
+        LocalStack stack = new LocalStack(size());
+        copyInto(stack);
+        return stack;
+    }
+
+    public void assertHeights(int[] heights) {
+        if (heights.length > size()) {
+            throw new IllegalArgumentException(String.format("hlen=%d, size=%d", heights.length, size()));
+        } else {
+            for (int i = 0; i < heights.length; i++) {
+                TypedLocal e = peek(i);
+                if (e.getB().getSize() != heights[i]) {
+                    throw new IllegalStateException(String.format("item at %d, len=%d, expected=%d, expr:%s", i,
+                            e.getB().getSize(), heights[i], e));
+                }
+            }
+        }
+    }
+
+    public void clear() {
+        for (int i = size - 1; i >= 0; i--) {
+            stack[i] = null;
+        }
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size <= 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int capacity() {
+        return stack.length;
+    }
+
+    public int height() {
+        int count = 0;
+        for (int i = 0; i < size(); i++) {
+            count += peek(i).getB().getSize();
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("top->btm[");
+        for (int i = size() - 1; i >= 0; i--) {
+            TypedLocal n = stack[i];
+            if (n != null) {
+                sb.append(n.getA());
+                sb.append(":").append(n.getB());
+                if (i != 0 && stack[i - 1] != null) {
+                    sb.append(", ");
+                }
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof LocalStack) {
+            LocalStack other = (LocalStack) o;
+            return other.size == size && Arrays.equals(other.stack, stack);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size, stack);
+    }
+
+    public static class TypedLocal extends ImmutableTuple2<Local, ValueType> {
+        public TypedLocal(Local fst, ValueType snd) {
+            super(fst, snd);
+        }
+    }
+}
---
+++ src/test/java/com/krakenrs/spade/ir/gen/LocalStackTest.java
@@ -0,0 +1,302 @@
+package com.krakenrs.spade.ir.gen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+import com.krakenrs.spade.ir.gen.LocalStack.TypedLocal;
+import com.krakenrs.spade.ir.type.PrimitiveType;
+import com.krakenrs.spade.ir.type.ValueType;
+import com.krakenrs.spade.ir.value.Local;
+
+public class LocalStackTest {
+
+    private static TypedLocal item(int size) {
+        ValueType type;
+        if(size == 2) {
+            type = PrimitiveType.DOUBLE;
+        } else {
+            type = PrimitiveType.INT;
+        }
+        return new TypedLocal(new Local(1, true), type);
+    }
+
+    @Test
+    public void testPush() {
+        var stack = new LocalStack();
+
+        assertEquals(0, stack.size());
+        assertTrue(stack.isEmpty());
+        stack.push(item(1));
+        stack.push(item(1));
+        stack.push(item(1));
+        assertEquals(3, stack.size());
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void testPushDouble() {
+        var stack = new LocalStack();
+        assertEquals(0, stack.size());
+        assertEquals(0, stack.height());
+        stack.push(item(1));
+        assertEquals(1, stack.size());
+        assertEquals(1, stack.height());
+        stack.push(item(2));
+        assertEquals(2, stack.size());
+        assertEquals(3, stack.height());
+    }
+
+    @Test
+    public void testPushOverflowCapacity() {
+        final int initialCapacity = 4;
+        final int numItems = initialCapacity + 1;
+        
+        var stack = new LocalStack(initialCapacity);
+        for (int i = 0; i < numItems; i++) {
+            stack.push(item(1));
+        }
+        assertEquals(numItems, stack.size());
+        assertTrue(stack.capacity() > numItems);
+    }
+
+    @Test
+    public void testPeekArguments() {
+        var stack = new LocalStack();
+        assertThrows(UnsupportedOperationException.class, () -> stack.peek());
+        assertThrows(IllegalArgumentException.class, () -> stack.peek(-1));
+    }
+
+    @Test
+    public void testPeek() {
+        var stack = new LocalStack();
+
+        var top = item(1);
+        var bottom = item(1);
+        stack.push(bottom);
+        stack.push(top);
+
+        assertEquals(top, stack.peek());
+        assertEquals(top, stack.peek(0));
+        assertEquals(bottom, stack.peek(1));
+        stack.pop();
+        assertEquals(bottom, stack.peek());
+        assertEquals(bottom, stack.peek(0));
+        assertThrows(UnsupportedOperationException.class, () -> stack.peek(1));
+    }
+
+    @Test
+    public void testPeekDouble() {
+        var stack = new LocalStack();
+        var top = item(2);
+        var bottom = item(1);
+        stack.push(bottom);
+        stack.push(top);
+        
+        assertEquals(top, stack.peek(0));
+        assertEquals(bottom, stack.peek(1));
+        assertThrows(UnsupportedOperationException.class, () -> stack.peek(2));
+    }
+
+    @Test
+    public void testPop() {
+        var stack = new LocalStack();
+        var items = new TypedLocal[5];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = item(1);
+            stack.push(items[i]);
+        }
+        assertEquals(items.length, stack.size());
+
+        for (int i = 0; i < items.length; i++) {
+            assertEquals(items.length - i, stack.size());
+            stack.pop();
+            assertEquals(items.length - i - 1, stack.size());
+        }
+    }
+
+    @Test
+    public void testPopDouble() {
+        var stack = new LocalStack();
+        var items = new TypedLocal[5];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = item(2);
+            stack.push(items[i]);
+        }
+        assertEquals(items.length, stack.size());
+        assertEquals(2 * items.length, stack.height());
+
+        for (int i = 0; i < items.length; i++) {
+            assertEquals(items.length - i, stack.size());
+            assertEquals(2 * (items.length - i), stack.height());
+            stack.pop();
+            assertEquals(items.length - i - 1, stack.size());
+            assertEquals(2 * (items.length - i - 1), stack.height());
+        }
+    }
+
+    @Test
+    public void testPopUnderflow() {
+        final int numElements = 3;
+        var stack = new LocalStack();
+
+        for (int i = 0; i < numElements; i++) {
+            stack.push(item(1));
+        }
+        assertEquals(numElements, stack.size());
+        for (int i = 0; i < numElements; i++) {
+            stack.pop();
+        }
+        assertEquals(0, stack.size());
+        assertThrows(UnsupportedOperationException.class, () -> stack.pop());
+    }
+
+    @Test
+    public void testGetAtArguments() {
+        var stack = new LocalStack();
+        assertThrows(IllegalArgumentException.class, () -> stack.getAt(-1));
+        assertThrows(IllegalArgumentException.class, () -> stack.getAt(0));
+
+        stack.push(item(1));
+        assertThrows(IllegalArgumentException.class, () -> stack.getAt(1));
+    }
+
+    @Test
+    public void testGetAt() {
+        var stack = new LocalStack();
+        var items = new TypedLocal[5];
+
+        for (int i = 0; i < items.length; i++) {
+            items[i] = item(1);
+            stack.push(items[i]);
+        }
+
+        for (int i = 0; i < items.length; i++) {
+            assertSame(items[i], stack.getAt(i));
+        }
+    }
+
+    @Test
+    public void testGetAtDouble() {
+        var stack = new LocalStack();
+        var items = new TypedLocal[] {
+                item(1), item(2), item(2), item(1), item(2)
+        };
+
+        for (int i = 0; i < items.length; i++) {
+            stack.push(items[i]);
+        }
+
+        for (int i = 0; i < items.length; i++) {
+            assertSame(items[i], stack.getAt(i));
+        }
+    }
+
+    @Test
+    public void testCopy() {
+        var s1 = new LocalStack();
+        s1.push(item(1));
+        s1.push(item(2));
+        s1.push(item(1));
+        s1.push(item(1));
+        s1.push(item(2));
+        var s2 = s1.copy();
+
+        assertEquals(s1, s2);
+    }
+
+    @Test
+    public void testNotEquals() {
+        var s1 = new LocalStack();
+        s1.push(item(1));
+        s1.push(item(2));
+        s1.push(item(1));
+        s1.push(item(1));
+        s1.push(item(2));
+        var s2 = s1.copy();
+        s2.pop();
+
+        assertNotEquals(s1, s2);
+    }
+
+    @Test
+    public void testAssertHeightsArguments() {
+        var stack = new LocalStack();
+        assertThrows(IllegalArgumentException.class, () -> stack.assertHeights(new int[] { 1 }));
+        stack.push(item(1));
+        assertThrows(IllegalArgumentException.class, () -> stack.assertHeights(new int[] { 1, 1 }));
+    }
+
+    @Test
+    public void testAssertHeightsEmpty() {
+        var stack = new LocalStack();
+        assertDoesNotThrow(() -> stack.assertHeights(new int[0]));
+    }
+    
+    @Test
+    public void testAssertHeightsEntireStack() {
+        var stack = new LocalStack();
+        stack.push(item(2));
+        stack.push(item(1));
+        stack.push(item(1));
+        int[] heights = { 1, 1, 2 };
+        assertDoesNotThrow(() -> stack.assertHeights(heights));
+    }
+
+    @Test
+    public void testAssertHeightsSubStack() {
+        var stack = new LocalStack();
+        stack.push(item(1));
+        stack.push(item(2));
+        stack.push(item(1));
+        stack.push(item(1));
+        int[] heights = { 1, 1, 2 };
+        assertDoesNotThrow(() -> stack.assertHeights(heights));
+    }
+
+    @Test
+    public void testAssertHeightsMismatch() {
+        var stack = new LocalStack();
+        stack.push(item(1));
+        stack.push(item(2));
+        stack.push(item(1));
+        stack.push(item(2));
+        int[] heights = { 1, 1, 2 };
+        assertThrows(IllegalStateException.class, () -> stack.assertHeights(heights));
+    }
+
+    @Test
+    public void testClear() {
+        var stack = new LocalStack();
+        assertEquals(0, stack.size());
+        for (int i = 0; i < 9; i++) {
+            stack.push(item(1));
+        }
+        stack.clear();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    public void testToStringEmpty() {
+        var stack = new LocalStack();
+        assertEquals("top->btm[]", stack.toString());
+    }
+
+    @Test
+    public void testToStringSingle() {
+        var stack = new LocalStack();
+        stack.push(item(1));
+        assertEquals("top->btm[svar1:I]", stack.toString());
+    }
+
+    @Test
+    public void testToStringMultiple() {
+        var stack = new LocalStack();
+        stack.push(item(1));
+        stack.push(item(1));
+        stack.push(item(2));
+        stack.push(item(2));
+        assertEquals("top->btm[svar1:D, svar1:D, svar1:I, svar1:I]", stack.toString());
+    }
+}
