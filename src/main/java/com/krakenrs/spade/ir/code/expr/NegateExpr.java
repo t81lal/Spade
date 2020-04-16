99d65d49e7d7a0e19c2a9cdac97d32a3a6d5e764
t81lal <bilal.a.tariq@gmail.com>
2020-04-16 20:29:19 +0100

    ir: code: change NegateExpr constructor

--- src/main/java/com/krakenrs/spade/ir/code/expr/NegateExpr.java
+++ src/main/java/com/krakenrs/spade/ir/code/expr/NegateExpr.java
@@ -5,13 +5,12 @@ import java.util.Objects;
 import com.krakenrs.spade.ir.code.Expr;
 import com.krakenrs.spade.ir.code.Opcodes;
 import com.krakenrs.spade.ir.code.expr.value.LoadLocalExpr;
-import com.krakenrs.spade.ir.type.ValueType;
 
 public class NegateExpr extends Expr {
     private final LoadLocalExpr var;
 
-    public NegateExpr(ValueType type, LoadLocalExpr var) {
-        super(Opcodes.NEGATE, type);
+    public NegateExpr(LoadLocalExpr var) {
+        super(Opcodes.NEGATE, var.type());
         this.var = var;
     }
 
