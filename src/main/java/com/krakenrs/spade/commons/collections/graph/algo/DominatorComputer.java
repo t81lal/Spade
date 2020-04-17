ad397accffb3f045a98cbaebf16f85fdff176860
t81lal <bilal.a.tariq@gmail.com>
2020-04-17 17:19:50 +0100

    commons: graph: algo: add comments to DominatorComputer

--- src/main/java/com/krakenrs/spade/commons/collections/graph/algo/DominatorComputer.java
+++ src/main/java/com/krakenrs/spade/commons/collections/graph/algo/DominatorComputer.java
@@ -7,6 +7,9 @@ import com.krakenrs.spade.commons.collections.graph.Vertex;
 
 import java.util.*;
 
+/* Thomas Lengauer and Robert Endre Tarjan. A fast algorithm for finding dominators
+ * in a flowgraph. ACM Transactions on Programming Languages and Systems
+ * (TOPLAS), 1(1):121–141,1979.  [pp. 121-131]. */
 public class DominatorComputer<V extends Vertex> {
     private final Digraph<V, ? extends Edge<V>> graph;
     private final V root;
@@ -15,33 +18,28 @@ public class DominatorComputer<V extends Vertex> {
     // before semidominators are computed: semi[w] = vertex[w]
     // after semidominators are computed: semi[w] = the semidominator of w
     private final Map<V, Integer> semi;
-
-    // vertex[i] = vertex of dfs pre-time i
+    // vertex[i] = vertex of dfs pre-time == i
     private final Map<Integer, V> vertex;
-
     private final List<V> postOrder;
-
     // parent[w] = parent of w in the dfs spanning tree
     private final Map<V, V> parent;
-
-    // idom(w) = immediate dominator of w after step 4
+    // idom[w] = immediate dominator of w after step 4
     private final Map<V, V> idoms;
-
-    // bucket[w] = set of vertices with a semidominator = w
+    // bucket[w] = set of vertices whose semidominator = w
     private final Map<V, Set<V>> bucket;
 
     private final Map<V, V> ancestor;
-
     private final Map<V, V> label;
 
+    /* treeDescendants(n) = descendants of n in the dominator tree + n */
     private final Map<V, Set<V>> treeDescendants;
-
+    /* treeSuccessors(n) = direct successors of n in the */
     private final Map<V, Set<V>> treeSuccessors;
 
     private final Map<V, Set<V>> frontiers;
-
     private final Map<V, Set<V>> iteratedFrontiers;
 
+    /* graph representation of the dominator tree. edges are of the form(idom(n), n) */
     private DominatorTree<V> dominatorTree;
 
     public DominatorComputer(
@@ -66,7 +64,12 @@ public class DominatorComputer<V extends Vertex> {
 
     public void run() {
         step1();
+        /* carry out step 2 and 3 on all w != r ∈ V in decreasing order by number.
+         * maintain a forest of vertex set V and edge set {(parent(w), w) | w ∈
+         * processed_nodes()} */
         step2and3();
+        /* step 4 examines vertices in increasing order by number, filling in the
+        * immediate dominators not explicitly computed in step 3. */
         step4();
 
         dominatorTree = makeDominatorTree();
@@ -112,6 +115,7 @@ public class DominatorComputer<V extends Vertex> {
     }
 
     private void step2and3() {
+        /* ignore entry */
         for (int i = semi.size() - 1; i > 0; i--) {
             V w = vertex.get(i);
             step2(w);
@@ -120,12 +124,15 @@ public class DominatorComputer<V extends Vertex> {
     }
 
     private void step2(V w) {
+        /* Theorem 4: For any vertex w != r: sdom(w) = min({v | (v, w} ∈ E and v < w} ∪
+         * {sdom(u) |u > w and ∃(v, w) such that u ->* v}) */
+
         boolean visitedAny = false;
 
         for (Edge<V> pred : graph.getReverseEdges(w)) {
             V v = pred.getSource();
 
-            // Ignore unreachable.
+            // Ignore unreachable
             if (!semi.containsKey(v)) {
                 continue;
             }
@@ -139,14 +146,24 @@ public class DominatorComputer<V extends Vertex> {
 
         if (visitedAny) {
             bucket.get(vertex.get(semi.get(w))).add(w);
-            ancestor.put(w, parent.get(w));
+            link(parent.get(w), w);
         }
     }
 
     private void step3(V w) {
+        /* Corollary 1: Let w != r and let u be a vertex for which sdom(u) is a minimum
+         * among vericies u satisfying sdom(w) ->+ u ->* w, then: idom(w) = sdom(w); if
+         * sdom(w) == sdom(u) idom(w) = idom(u); otherwise
+         * 
+         * implicitly define the immediate dominator of each vertex by applying corollary 1. */
+
         Set<V> wbucket = bucket.get(parent.get(w));
         for (V v : wbucket) {
             V u = eval(v);
+            /* If the semidominator of w is its immediate dominator, then dom is the
+             * immediate dominator of w. Otherwise dom is a vertex, v, whose number is
+             * smaller than w and whose immediate dominator is also w's immediate dominator. */
+
             V dom = semi.get(u) < semi.get(v) ? u : parent.get(w);
             idoms.put(v, dom);
         }
@@ -154,6 +171,9 @@ public class DominatorComputer<V extends Vertex> {
     }
 
     private void step4() {
+        /* explicitly define the immediate dominator of each vertex, carrying out the
+         * computation vertex by vertex in increasing order by number. */
+
         for (int i = 0; i < semi.size(); i++) {
             V w = vertex.get(i);
             if (idoms.get(w) != vertex.get(semi.get(w))) {
@@ -161,6 +181,15 @@ public class DominatorComputer<V extends Vertex> {
             }
         }
     }
+
+    /* add (v, w) to the forest */
+    private void link(V v, V w) {
+        ancestor.put(w, v);
+    }
+
+    /* if v is a root of a tree in the forest, return v. else let r be the root of
+     * the tree in the forest which contains v. return any vertex u != r of minimum
+     * semi(u) on the path r ->* v */
 
     private V eval(V v) {
         if (ancestor.containsKey(v)) {
@@ -203,11 +232,9 @@ public class DominatorComputer<V extends Vertex> {
     }
 
     private List<V> treeReverseTopoOrder() {
-        /*
-         * side notes: topo sort on tree == pre order on tree (not on general DAG) ==
+        /* side notes: topo sort on tree == pre order on tree (not on general DAG) ==
          * reverse post order on general DAG, therefore reverse topo sort on tree ==
-         * post order of general DAG
-         */
+         * post order of general DAG */
         DepthFirstSearch<V> dfs = new DepthFirstSearch<>(getDominatorTree());
         dfs.run(root);
         return dfs.getPostOrder();
