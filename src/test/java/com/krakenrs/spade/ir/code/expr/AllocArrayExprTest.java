118af84a0a635304f85d04572ae466e838d9cd19
Bilal Tariq <biblthebest@hotmail.com>
2020-07-11 16:12:14 +0100

    ir: code: add AllocArrayExpr tests

--- src/test/java/com/krakenrs/spade/ir/code/expr/AllocArrayExprTest.java
+++ src/test/java/com/krakenrs/spade/ir/code/expr/AllocArrayExprTest.java
@@ -2,23 +2,38 @@ package com.krakenrs.spade.ir.code.expr;
 
 import static com.krakenrs.spade.ir.code.StmtTests.child;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import com.krakenrs.spade.ir.code.expr.value.ValueExpr;
+import com.krakenrs.spade.ir.code.visitor.CodeReducer;
+import com.krakenrs.spade.ir.code.visitor.CodeVisitor;
 import com.krakenrs.spade.ir.type.PrimitiveType;
 
 public class AllocArrayExprTest {
     @Test
-    void testConstructor() {
+    void testInvalidConstructor() {
         assertThrows(NullPointerException.class, () -> new AllocArrayExpr(PrimitiveType.INT, null));
         assertThrows(NullPointerException.class, () -> new AllocArrayExpr(null, List.of(child())));
 
         // empty bounds
         assertThrows(IllegalArgumentException.class, () -> new AllocArrayExpr(PrimitiveType.INT, List.of()));
+    }
+
+    @Test
+    void testGetters() {
+        List<ValueExpr<?>> bounds = List.of(child(), child());
+        var expr = new AllocArrayExpr(PrimitiveType.INT, bounds);
+        assertEquals(PrimitiveType.INT, expr.getType());
+        assertEquals(bounds, expr.getBounds());
     }
 
     @Test
@@ -28,5 +43,42 @@ public class AllocArrayExprTest {
         for (var c : children) {
             assertEquals(expr, c.getParent());
         }
+    }
+
+    @Test
+    void testCodeVisitor() {
+        var expr = new AllocArrayExpr(PrimitiveType.INT, List.of(child()));
+        CodeVisitor vis = mock(CodeVisitor.class);
+        expr.accept(vis);
+
+        verify(vis).visitAny(expr);
+        verify(vis).visitAllocArrayExpr(expr);
+        verifyNoMoreInteractions(vis);
+    }
+
+    @Test
+    void testCodeReducer() {
+        var expr = new AllocArrayExpr(PrimitiveType.INT, List.of(child()));
+        CodeReducer red = mock(CodeReducer.class);
+        expr.reduceExpr(red);
+
+        verify(red).reduceAllocArrayExpr(expr);
+        verifyNoMoreInteractions(red);
+    }
+
+    @Test
+    void testEquivalent() {
+        var e1 = new AllocArrayExpr(PrimitiveType.INT, List.of(child()));
+        var e2 = new AllocArrayExpr(PrimitiveType.INT, List.of(child()));
+
+        assertTrue(e1.equivalent(e2));
+    }
+
+    @Test
+    void testNotEquivalentBounds() {
+        var e1 = new AllocArrayExpr(PrimitiveType.INT, List.of(child(), child()));
+        var e2 = new AllocArrayExpr(PrimitiveType.INT, List.of(child()));
+
+        assertFalse(e1.equivalent(e2));
     }
 }
