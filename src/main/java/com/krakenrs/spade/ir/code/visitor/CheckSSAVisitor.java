a6d0323075d4186482c316d25498e57744946794
Bilal Tariq <biblthebest@hotmail.com>
2020-07-14 17:37:49 +0100

    ir: code: visitor: fix CheckSSAVisitor to include decls

--- src/main/java/com/krakenrs/spade/ir/code/visitor/CheckSSAVisitor.java
+++ src/main/java/com/krakenrs/spade/ir/code/visitor/CheckSSAVisitor.java
@@ -2,6 +2,10 @@ package com.krakenrs.spade.ir.code.visitor;
 
 import com.krakenrs.spade.ir.code.expr.value.LoadLocalExpr;
 import com.krakenrs.spade.ir.code.expr.value.ValueExpr;
+import com.krakenrs.spade.ir.code.stmt.AssignCatchStmt;
+import com.krakenrs.spade.ir.code.stmt.AssignLocalStmt;
+import com.krakenrs.spade.ir.code.stmt.AssignParamStmt;
+import com.krakenrs.spade.ir.code.stmt.AssignPhiStmt;
 import com.krakenrs.spade.ir.value.Local;
 
 public class CheckSSAVisitor extends AbstractValueVisitor<Boolean> {
@@ -11,14 +15,43 @@ public class CheckSSAVisitor extends AbstractValueVisitor<Boolean> {
 		isSSA = true;
 	}
 
+	private void mergeLocal(Local local) {
+		if (!local.isVersioned()) {
+			isSSA = false;
+		}
+	}
+
+	@Override
+	public void visitAssignLocalStmt(AssignLocalStmt s) {
+		super.visitAssignLocalStmt(s);
+		mergeLocal(s.var());
+	}
+
+	@Override
+	public void visitAssignParamStmt(AssignParamStmt s) {
+		super.visitAssignParamStmt(s);
+		mergeLocal(s.var());
+	}
+
+	@Override
+	public void visitAssignCatchStmt(AssignCatchStmt s) {
+		super.visitAssignCatchStmt(s);
+		mergeLocal(s.var());
+	}
+
 	@Override
 	public void visitValueExpr(ValueExpr<?> e) {
+		super.visitValueExpr(e);
 		if (e instanceof LoadLocalExpr) {
-			Local local = (Local) e.value();
-			if (!local.isVersioned()) {
-				isSSA = false;
-			}
+			mergeLocal((Local) e.value());
 		}
+	}
+
+	@Override
+	public void visitAssignPhiStmt(AssignPhiStmt s) {
+		super.visitAssignPhiStmt(s);
+		s.arguments().values().forEach(this::mergeLocal);
+		mergeLocal(s.var());
 	}
 
 	@Override
