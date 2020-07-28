42178b91b0e9f0dd1feffd79b60e81e646e16e1e
Bilal Tariq <biblthebest@hotmail.com>
2020-07-28 01:18:04 +0100

    app: asm: add KlassScene

---
+++ src/main/java/com/krakenrs/spade/app/asm/KlassScene.java
@@ -0,0 +1,51 @@
+package com.krakenrs.spade.app.asm;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.NonNull;
+
+public class KlassScene {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KlassScene.class);
+
+    private final Collection<KlassSource> sources;
+
+    public KlassScene() {
+        sources = new HashSet<>();
+    }
+
+    public Klass findKlass(@NonNull String name) {
+        name = name.replace(".", "/");
+        for (KlassSource source : sources) {
+            if (source.hasKlass(name)) {
+                LOGGER.info("Found loaded klass: {}", name);
+                return source.getKlass(name);
+            }
+        }
+        LOGGER.info("Couldn't find loaded klass: {}", name);
+        return null;
+    }
+
+    private void addSource(KlassSource source) {
+        sources.add(source);
+    }
+
+    public void addJarSource(File file, int asmOptions) throws IOException {
+        LOGGER.info("Loading classes from: {}", file.toString());
+        String name = file.getName();
+        addJarSource(name, new FileInputStream(file), asmOptions);
+    }
+
+    public void addJarSource(String name, InputStream is, int asmOptions) throws IOException {
+        KlassSource source = new KlassSource(name);
+        source.loadFromJar(is, asmOptions, true);
+        addSource(source);
+    }
+}
